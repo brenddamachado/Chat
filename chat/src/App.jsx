6e23b604ff60c186bf158654c6515f4ddb25646d
brenddamachado <brenda16machado.bm@gmail.com>
2023-04-19 14:57:15 -0300

    atualização no app

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -1,11 +1,18 @@
-import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import {useAuthState, useSignInWithGoogle} from 'react-firebase-hooks/auth';
 import './App.css';
-
+import { auth } from './serves/firebaseConfig';
 
 export const App = () => {
-  return (<div>
-    
-  </div>)
+  const [user] = useAuthState(auth);
+  return (
+    <div className="App">
+      <header>
+        <h1> ReactChat</h1>
+        <SignOut/>
+      </header>
+      <section></section>
+    </div>
+    )
 }
 
 
@@ -14,8 +21,19 @@ export const ChatRoom= () => {
 }
 
 
-export const SignOut = () => {
-  const [ ]= useSignInWithGoogle(auth);
-  return <button className='
-  sign-in' onClick={() => useSignInWithGoogle()}> Logar com Google</button>;
+export const SignIn = () => {
+  const [useSignInWithGoogle ]= useSignInWithGoogle(auth);
+  return (<button className='
+  sign-in' onClick={() => useSignInWithGoogle()}> Logar com Google
+  </button>
+  );
 };
+
+export const SignOut= () => {
+  return auth.currentUser && <button className='sign-out'
+   onClick={() => auth.SignOut()}>
+    sair
+  </button> ;
+   
+  
+}
--- chat/src/serves/firebaseConfig.js
+++ chat/src/serves/firebaseConfig.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getAuth} from 'firebase/auth';
 import { getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,5 +20,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-const analytics = getAnalytics(app);
-export const dataabaseApp = getFirestore (app);
+export const databaseApp = getFirestore (app);
