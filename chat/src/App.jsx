924c3ea516c82dbc89d8efca98bed7e56805b335
brenddamachado <brenda16machado.bm@gmail.com>
2023-04-19 15:52:21 -0300

    chatRoom

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -1,6 +1,7 @@
 import {useAuthState, useSignInWithGoogle} from 'react-firebase-hooks/auth';
 import './App.css';
-import { auth } from './serves/firebaseConfig';
+import { auth, databaseApp } from './serves/firebaseConfig';
+import { collection, orderBy } from 'firebase/firestore';
 
 export const App = () => {
   const [user] = useAuthState(auth);
@@ -19,9 +20,40 @@ export const App = () => {
 
 
 export const ChatRoom= () => {
-  return <h1>ChatRoom</h1>
-}
+ const menssageRef = collection(databaseApp, 'messages')
+ const QueryMessagens = query(menssageRef, orderBy('createdAt'), limit(25));
+ const [messages] = useCollectionData(QueryMessagens, {IdField: 'id'});
+ const [formValue, setformValue] = useState('');
+ const sendMessage = (e) => {
+  e.preventDefault();
+  
+ }
+ 
+ return(
+  <>
+  <main>
+    {
+      messages && messages.map((msg, index) => (
+       <ChatMessage key={index}  menssage={msg}/>)
+      )
+    }
+  </main>
+  
+  </>
+ )
+};
 
+export const ChatMessage= (props) => {
+  const {text, photoURL, uid} = props.menssage;
+  const menssageClass = uid === auth.currentUser.uid ? 'send' : 'received';
+  return (
+<div className={`menssage ${menssageClass}`}>
+  <img src= {photoURL}/>
+  <p>{text}</p>
+</div>
+  )
+ 
+};
 
 export const SignIn = () => {
   const [signInWithGoogle] = useSignInWithGoogle(auth);
